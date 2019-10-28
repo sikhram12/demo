b9b47616a29ca6c21239523c3030790f788e0541
anil_sikhram <anil.sikhram@maryland.gov>
2019-10-28 10:54:20 -0400

    fixed build issue

--- src/main/java/com/sparkle/demo/DemoApplication.java
+++ src/main/java/com/sparkle/demo/DemoApplication.java
@@ -1,10 +1,9 @@
-package com.dhs.demo;
+package com.sparkle.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 //@CrossOrigin(origins = "http://localhost:8080")
--- src/main/java/com/sparkle/demo/service/EmployeeService.java
+++ src/main/java/com/sparkle/demo/service/EmployeeService.java
@@ -1,6 +1,5 @@
 package com.sparkle.demo.service;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
