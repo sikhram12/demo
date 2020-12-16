16c2a4311132e6e596e26cdb0f3fa3634eb161f9
anil_sikhram <anil.sikhram@maryland.gov>
2020-12-16 13:20:30 -0500

    Added new AWS mongo db properties

--- src/main/java/com/sparkle/demo/model/UserDetails.java
+++ src/main/java/com/sparkle/demo/model/UserDetails.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Data;
 
-@Document(collection = "documents")
+@Document(collection = "users")
 @Data
 public class UserDetails {
 	
