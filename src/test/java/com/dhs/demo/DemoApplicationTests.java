818d23a316fa874af9231d643d91260cab911c56
unknown <oohaksbn05@gmail.com>
2021-10-24 19:24:09 +0530

    test file commit

--- src/test/java/com/dhs/demo/DemoApplicationTests.java
+++ src/test/java/com/dhs/demo/DemoApplicationTests.java
@@ -10,7 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads()
+	{
+		System.out.println("test class");
 	}
 
 }
